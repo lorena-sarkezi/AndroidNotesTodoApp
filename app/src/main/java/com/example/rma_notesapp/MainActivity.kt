f62693a1515c298709303c1a1cef2ff20a9fadd9
lorena-sarkezi <lorena.sarkezi@gmail.com>
2021-01-25 22:09:14 +0100

    Add GitHub link Implicit activity

--- app/src/main/java/com/example/rma_notesapp/MainActivity.kt
+++ app/src/main/java/com/example/rma_notesapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.rma_notesapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +17,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
+    private val gitHubUri: String = "https://github.com/lorena-sarkezi"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -24,5 +29,22 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_notes, R.id.navigation_todos, R.id.navigation_archive))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //setSupportActionBar(findViewById(R.id.action_github))
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val itemId = item.itemId
+
+        if(item.itemId == R.id.action_github){
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(gitHubUri))
+            startActivity(browserIntent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
